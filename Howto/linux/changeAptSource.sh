962ba581b8d454d40f73f479ea95488fab262d13
Jeff Fang <all2jeff@gmail.com>
2018-06-09 08:16:17 +0800

    add file

---
+++ Howto/linux/changeAptSource.sh
@@ -0,0 +1,25 @@
+
+# BACKUP THIS FILE BEFORE CHANGING IT!
+# sudo cp /etc/apt/sources.list sources.list.bak
+
+
+deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse  
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse  
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse  
+deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse  
+
+## Test version source 
+deb http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse  
+
+# source code source  
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse  
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse  
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse  
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse  
+
+## test version source
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse  
+
+# Canonical partner source  
+deb http://archive.canonical.com/ubuntu/ xenial partner  
+deb http://extras.ubuntu.com/ubuntu/ xenial main  
