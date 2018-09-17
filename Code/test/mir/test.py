4f89bca9aff86fe1b7482946a0fa83c23d9e8409
Jeff Fang <all2jeff@gmail.com>
2018-09-17 23:46:46 +0800

    update

--- Code/test/mir/test.py
+++ Code/test/mir/test.py
@@ -3,28 +3,36 @@ from time import sleep
 
 url = "http://192.168.1.101/api/v2.0.0/status"
 pl_continue = "{\n\t\"state_id\": 3 \n}"
-pl_pause= "{\n\t\"state_id\": 4 \n}"
+pl_pause = "{\n\t\"state_id\": 4 \n}"
 
 headers = {
     'content-type': "application/json",
-    'authorization': "Basic ZGlzdHJpYnV0b3I6NjJmMmYwZjFlZmYxMGQzMTUyYzk1ZjZmMDU5NjU3NmU0ODJiYjhlNDQ4MDY0MzNmNGNmOTI5NzkyODM0YjAxNA==",
+    'authorization': "Basic ZGlzdHJpYnV0b3I6NjJmMmYwZjFlZmYxMGQzMTUyYzk1ZjZm" +
+                    "MDU5NjU3NmU0ODJiYjhlNDQ4MDY0MzNmNGNmOTI5NzkyODM0YjAxNA==",
     'cache-control': "no-cache",
     'postman-token': "00a40e04-cd97-f5c2-72c0-cf184bedafd8"
     }
+
 
 def rb_continue():
     response = requests.request("PUT", url, data=pl_continue, headers=headers)
     print(response.text)
     pass
 
+
 def rb_pause():
     response = requests.request("PUT", url, data=pl_pause, headers=headers)
     print(response.text)
     pass
 
-rb_pause()
-sleep(2)
-rb_continue()
-sleep(2)
-rb_pause()
+# rb_pause()
+# sleep(2)
+# rb_continue()
+# sleep(2)
+# rb_pause()
 
+keyfile = open("pad", 'r')
+for i in range(8):
+    keycode = keyfile.read(1)
+    print(keycode)
+keyfile.close()
