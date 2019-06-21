b5fcbcd71ea5cc0514087be862df65e277ee8e94
Jeff Fang <all2jeff@gmail.com>
2019-06-21 17:02:30 +0800

    add contents

---
+++ Code/test/socket/server.py
@@ -0,0 +1,17 @@
+import socket
+
+HOST = 'localhost'
+PORT = 50007
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            print("Server: Data received!")
+            if not data:
+                break
+            print(data)
--- Howto/tool/useConda.md
+++ Howto/tool/useConda.md
@@ -6,8 +6,9 @@ conda update --all
 conda list
 conda search beautifulsoup
 
-conda create -n py2 python=2.7
-source activate py2  
+conda create -n ENV_NAME python=2.7
+source activate ENV_NAME
+conda env remove -n ENV_NAME
 
 conda install ipykernel
 ipython kernel install --name kernel_name
