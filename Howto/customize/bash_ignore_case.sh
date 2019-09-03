3f7760c5e86d1a07c4491e925d81f668d21da719
Jeff Fang <all2jeff@gmail.com>
2019-09-02 18:55:56 -0700

    update

--- Howto/customize/bash_ignore_case.sh
+++
@@ -1,7 +0,0 @@
-# https://askubuntu.com/questions/87061/can-i-make-tab-auto-completion-case-insensitive-in-bash
-
-# If ~./inputrc doesn't exist yet, first include the original /etc/inputrc so we don't override it
-if [ ! -a ~/.inputrc ]; then echo '$include /etc/inputrc' > ~/.inputrc; fi
-
-# Add option to ~/.inputrc to enable case-insensitive tab completion
-echo 'set completion-ignore-case On' >> ~/.inputrc
