58e1ee13ceb10867c4a1153f939dd441cdf07a7c
Jeff Fang <all2jeff@gmail.com>
2019-09-04 18:00:12 +0800

    update

--- Howto/customize/rosenv/set_ROS_env.sh
+++ Howto/customize/rosenv/set_ROS_env.sh
@@ -2,50 +2,53 @@
 # If ROS environment is not been choosed, use Anaconda to setup python 3.
 # Setting up Gazebo path as well
 
-# Setting Gazebo Path
-if [ -f ~/.rosenv/set_gazebo_path.sh ]; then
-    . ~/.rosenv/set_gazebo_path.sh
-fi
-
 # Allow for ROS source choice
-if [ -n "$BASH_VERSION" ]; then
-  while true
-  do
+while true
+do
+  if [ -n "$BASH_VERSION" ]; then
     read -p "Do you want to source ROS in this workspace? (y/n): " input_choice
-
-    if [ "$input_choice" = "y" ]; then
-      . ~/.rosenv/set_catkin_env.sh
-      break
-    elif [ "$input_choice" = "n" ]; then
-      echo "ROS *NOT* sourced!"
-      # Setup conda
-      export PATH="/home/jeff/Application/anaconda3/bin:$PATH"
-      break
-    else
-      echo "Warning: Not an acceptable option. Choose (y/n).                                  "
-    fi
-  done
-
-
-elif [ -n "$ZSH_VERSION" ]; then
-  while true
-  do
+  elif [ -n "$ZSH_VERSION" ]; then
     read -q "input_choice?Do you want to source ROS in this workspace?"
     echo
+  fi
 
-    if [ "$input_choice" = "y" ]; then
-      . ~/.rosenv/set_catkin_env.sh
-      # Set up ROS ip
-      export ROS_IP=`echo $(hostname -I)`
-      break
-    elif [ "$input_choice" = "n" ]; then
-      echo "ROS *NOT* sourced!"
+  if [ "$input_choice" = "y" ]; then
+    . ~/.rosenv/set_catkin_env.sh
 
-      # Setup conda
-      export PATH="/home/jeff/Application/anaconda3/bin:$PATH"
-      break
-    else
-      echo "Warning: Not an acceptable option. Choose (y/n).                                  "
+    # export ROS_IP=`echo $(hostname -I)`
+    # export ROS_MASTER_URI=http://192.168.1.107:11311
+    # export ROS_HOSTNAME=192.168.1.107
+ 
+    # Setting Gazebo Path
+    if [ -f ~/.rosenv/set_gazebo_path.sh ]; then
+        . ~/.rosenv/set_gazebo_path.sh
     fi
-  done
-fi
+
+    # export TURTLEBOT3_MODEL=burger
+
+    break
+
+  elif [ "$input_choice" = "n" ]; then
+    echo "ROS *NOT* sourced!"
+    # Setup conda
+    # export PATH="/home/jeff/Application/anaconda3/bin:$PATH"
+
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    __conda_setup="$('/home/jeff/Application/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "/home/jeff/Application/anaconda3/etc/profile.d/conda.sh" ]; then
+            . "/home/jeff/Application/anaconda3/etc/profile.d/conda.sh"
+        else
+            export PATH="/home/jeff/Application/anaconda3/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
+    # <<< conda initialize <<<
+    break
+  else
+    echo "Warning: Not an acceptable option. Choose (y/n).                                  "
+  fi
+done
--- Howto/customize/rosenv/set_catkin_env.sh
+++ Howto/customize/rosenv/set_catkin_env.sh
@@ -1,4 +1,4 @@
-
+export catkin_ws=$HOME/catkin_ws
 # export catkin_ws=$HOME/Udacity/robond_p2/catkin_ws
 # export catkin_ws=$HOME/Udacity/robond_p3/catkin_ws
 # export catkin_ws=$HOME/Udacity/robond_p4/catkin_ws
@@ -7,13 +7,15 @@
 # export catkin_ws=$HOME/catkin_ws4
 # export catkin_ws=$HOME/Workspace/Udacity/ros/catkin101
 # export catkin_ws=$HOME/Workspace/Udacity/proj2/catkin_ws
-export catkin_ws=$HOME/Workspace/Udacity/proj4/ekf_ws
+# export catkin_ws=$HOME/Workspace/Udacity/proj4/ekf_ws
 
 if [ -n "$BASH_VERSION" ]; then
+  # source /opt/ros/kinetic/setup.bash
   source /opt/ros/melodic/setup.bash
   source $catkin_ws/devel/setup.bash
 
 elif [ -n "$ZSH_VERSION" ]; then
+  # source /opt/ros/kinetic/setup.zsh
   source /opt/ros/melodic/setup.zsh
   source $catkin_ws/devel/setup.zsh
 fi
